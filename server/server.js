3df3f73b564e31c4468c486b6484e91ddc60a331
Narin Sundarabhaya <nrs710@gmail.com>
2018-07-18 00:02:30 -0700

    feature(Client): Add Client-side * Add client-side from create-react-app-typescript * Remove unused from cra * Add scripts * Add tslint * Add Husky & lint-staged to server side * Update server side to es6 * Add babel on server side

--- server/server.js
+++ server/server.js
@@ -1,27 +1,31 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import path from 'path';
+import dotenv from 'dotenv';
+
+const port = process.env.PORT | 3001;
 const app = express();
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const port = process.env.PORT;
-const logger = require('morgan');
-const mongoose = require('mongoose');
-require('dotenv').config();
+
+dotenv.config();
 
 // Routes
-const temp = require('./routes/api/temp_prob');
+import temp from './routes/api/temp_prob';
 
-app.use(logger('dev'));
+app.use(morgan('dev'));
 
 mongoose.Promise = Promise;
 mongoose
-    .connect(process.env.MONGO_URI, { useNewUrlParser: true })
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.error(err));
+	.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+	.then(() => console.log('MongoDB Connected'))
+	.catch(err => console.error(err));
 
 app.use(
-    bodyParser.urlencoded({
-        extended: false,
-    }),
+	bodyParser.urlencoded({
+		extended: false,
+	}),
 );
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -30,6 +34,16 @@ app.use(bodyParser.text());
 // Routes
 app.use('/api/temp', temp);
 
+//  Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(`${__dirname}/../client/build`));
 
-app.use((req, res) => res.status(404).send("Sorry can't find that!"));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, '../client/build/index.html'));
+	});
+}
+
+app.use((req, res) => res.status(404).send('Sorry can\'t find that!'));
 app.listen(port, () => console.log(`==> 🌎  Listening on PORT ${port}. Visit http://localhost:${port}`));
+
+export default app;
